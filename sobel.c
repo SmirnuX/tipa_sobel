278a910a27aa224384d61f74353cc94e9055b467
Смирнов Василий <xomafok@gmail.com>
2021-03-13 02:17:51 +0300

    Added timer and fixed some bugs
    Signed-off-by: Смирнов Василий <xomafok@gmail.com>

--- sobel.c
+++ sobel.c
@@ -5,20 +5,50 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
+#include <time.h>
 #include <math.h>
 
+int core[3][3] = {	{ -1, -2, -1 },
+					{  0,  0,  0 },
+					{  1,  2,  1 }};
+/*
+int core[3][3] = { 	{  -3, -10,  -3},
+					{   0,   0,   0},
+					{   3,  10,   3}}; 
+*/
 
-char core[] = {1, 2, 1};
+float intensity[3]	=	{0.2126, 0.7152, 0.0722};	//Интенсивность разных цветов
+//float intensity[3] = {0.33, 0.33, 0.33};
 char brightness(char r, char g, char b);	//Яркость точки 
+int convolution(char** arr, char direction, int x, int y);	//Свертка изображения.
+
+//Аргументы:
+//tsobel <input file> <output file> [threads]
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	int thread_count = 1;
+	if (argc < 3)
 	{
-		printf("А где аргумент\n");
+		printf("Недостаточно аргументов.\n");
 		return 1;
 	}
+	if (argc > 3)
+	{
+		thread_count = atoi(argv[3]);
+		if (thread_count < 1)	//TODO - максимальное кол-во потоков
+		{
+			printf("Непрально указано количество потоков.\n");
+			return 1;
+		}
+	}
 	int in_file = open(argv[1], O_RDONLY);
+	if (in_file < 0)
+	{
+		perror("Ошибка открытия файла.");
+		return errno;
+	}
 	int out_file = open(argv[2], O_CREAT | O_WRONLY, 0666);
 	//Все хранится в памяти
 	char** pic;
@@ -81,36 +111,50 @@ int main(int argc, char* argv[])
 			pic[j][i] = brightness(r, g, b);	//Запись в ч/б
 		}
 	}
-	//Запись производится следующим образом: края - черная рамка
+	//Непосредственно сама обработка
+	printf("Количество потоков: %i\n", thread_count);
+	clock_t start = clock();
 	for (int i = 0; i < h; i++)
 	{
 		char sobel = 0;			
 		for (int j = 0; j < w; j++)
 		{
-			if (i == 0 || i == h-1 || j == 0 || j == w-1)
+			if (i == 0 || i == h-1 || j == 0 || j == w-1)	//Края не обрабатываются
 				sobel = 0;
 			else
 			{
-				int gy = - core[0]*pic[j-1][i-1] - core[1]*pic[j][i-1] - core[2]*pic[j+1][i-1] + core[0]*pic[j-1][i+1] + core[1]*pic[j][i+1] + core[2]*pic[j+1][i+1];
-				int gx = - core[0]*pic[j-1][i-1] - core[1]*pic[j-1][i] - core[2]*pic[j-1][i+1] + core[0]*pic[j+1][i-1] + core[1]*pic[j+1][i] + core[2]*pic[j+1][i+1];
+				int gy = convolution(pic, 0, j, i);
+				int gx = convolution(pic, 1, j, i);
 				sobel = hypot(gx, gy);
 			}	
 			for (int k = 0; k < 3; k++)
 				write(out_file, &sobel, 1);
 		}	
 	}
-
+	double time = (double)(clock() - start) / CLOCKS_PER_SEC;
+	printf("Прошло %f с.", time);
 	//Освобождение памяти
 	for (int i = 0; i < w; i++)
 		free(pic[i]);
 	free(pic);
 	//TODO - многопоточность
-	//TODO - таймер
 	return 0;
 }
 
 char brightness(char r, char g, char b)	//Яркость точки 
 {
-	return (0.2126*r + 0.7152*g + 0.0722*b); 	
+	return r * intensity[0] + g * intensity[1] + b * intensity[2];
+}
+
+int convolution(char** arr, char direction, int x, int y)	//Свертка изображения
+{
+	int result = 0;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			if (direction == 0)	//Вертикальная свертка
+				result += core[i][j] * arr[x+i-1][y+i-1];
+			else
+				result += core[j][i] * arr[x+i-1][y+i-1];
+	return result;
 }
 
